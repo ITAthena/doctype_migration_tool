9cb4c8c5ba6a7df8d6102dbdb64ce17d22142d40
ITAthena <it@rsasurgicalcentre.co.uk>
2025-09-20 07:26:28 +0000

    changes

--- doctype_migration_tool/doctype_migration_tool/doctype/avi_test_2/avi_test_2.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Ameer Muavia and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Avi Test 2', {
-	// refresh: function(frm) {
-
-	// }
-});
--- doctype_migration_tool/doctype_migration_tool/doctype/avi_test_2/test_avi_test_2.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Ameer Muavia and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestAviTest2(FrappeTestCase):
-	pass
