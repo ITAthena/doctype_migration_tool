8e5199bc37a29698514b180f5b3c51bb1ed66b35
ITAthena <it@rsasurgicalcentre.co.uk>
2025-09-20 07:16:22 +0000

    changes

---
+++ doctype_migration_tool/doctype_migration_tool/doctype/avi_test_2/avi_test_2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ameer Muavia and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Avi Test 2', {
+	// refresh: function(frm) {
+
+	// }
+});
